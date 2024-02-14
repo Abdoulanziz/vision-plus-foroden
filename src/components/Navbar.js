831b31b3b3c7b3f0e53982f396995663fb74e632
Abdoulanziz <abdoulanzizally@outlook.com>
2024-02-14 12:55:16 +0300

    Fix bug on mobile nav modal that prevented the modal from closing after an item is clicked.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,81 +1,85 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-    
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    };
+
     return (
         <>
             <nav className="navbar navbar-expand-md fixed-top navbar-shrink py-3 navbar-light" id="mainNav">
                 <div className="container d-flex justify-content-between align-items-center">
-
                     <div className="col">
-                    <a href="/" className="navbar-brand">
-                        <img src="assets/img/logo/vision-plus-logo.svg" alt="Logo" className="navbar-logo" />
-                        <span></span>
-                    </a>
+                        <a href="/" className="navbar-brand">
+                            <img src="assets/img/logo/vision-plus-logo.svg" alt="Logo" className="navbar-logo" />
+                            <span></span>
+                        </a>
                     </div>
-
                     <div className="col-auto">
-                        <button className="navbar-toggler" type="button" data-bs-toggle="modal" data-bs-target="#menuModal">
+                        <button className="navbar-toggler" type="button" onClick={toggleModal}>
                             <span className="navbar-toggler-icon"></span>
                         </button>
                     </div>
-
                     <div className="collapse navbar-collapse justify-content-center" id="navcol-1">
                         <ul className="navbar-nav text-center">
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/">Home</NavLink>
+                                <NavLink className="nav-link" to="/" onClick={toggleModal}>Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/insights">Founders' Insights</NavLink>
+                                <NavLink className="nav-link" to="/insights" onClick={toggleModal}>Founders' Insights</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/concepts">Business Concepts</NavLink>
+                                <NavLink className="nav-link" to="/concepts" onClick={toggleModal}>Business Concepts</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/faqs">FAQs</NavLink>
+                                <NavLink className="nav-link" to="/faqs" onClick={toggleModal}>FAQs</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                                <NavLink className="nav-link" to="/contact" onClick={toggleModal}>Contact</NavLink>
                             </li>
                         </ul>
                         <NavLink className="btn btn-secondary navbar-btn" role="button" to="/">Featured Projects</NavLink>
                     </div>
                 </div>
             </nav>
-
-            <div className="modal fade" id="menuModal" tabIndex="-1" role="dialog" aria-labelledby="menuModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-top" role="document" style={{margin: "0", height: "100vh"}}>
-                    <div className="modal-content" style={{height: "100%", border: "none"}}>
-                        <div className="modal-header">
-                            <a href="/" className="navbar-brand">
-                                <img src="assets/img/logo/vision-plus-logo.svg" alt="Logo" className="navbar-logo" />
-                                <span></span>
-                            </a>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <ul className="navbar-nav mx-auto text-center">
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" data-dismiss="modal" to="/">Home</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/insights">Founders' Insights</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/concepts">Business Concepts</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/faqs">FAQs</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/contact">Contact</NavLink>
-                                </li>
-                                <NavLink className="btn btn-secondary navbar-btn" role="button" to="/">Featured Projects</NavLink>
-                            </ul>
+            {showModal && (
+                <div className="modal fade show" tabIndex="-1" role="dialog" style={{ display: "block" }}>
+                    <div className="modal-dialog modal-dialog-top" role="document" style={{ margin: "0", height: "100vh" }}>
+                        <div className="modal-content" style={{ height: "100%", border: "none" }}>
+                            <div className="modal-header">
+                                <a href="/" className="navbar-brand">
+                                    <img src="assets/img/logo/vision-plus-logo.svg" alt="Logo" className="navbar-logo" />
+                                    <span></span>
+                                </a>
+                                <button type="button" className="btn-close" onClick={toggleModal} aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <ul className="navbar-nav mx-auto text-center">
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/" onClick={toggleModal}>Home</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/insights" onClick={toggleModal}>Founders' Insights</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/concepts" onClick={toggleModal}>Business Concepts</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/faqs" onClick={toggleModal}>FAQs</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/contact" onClick={toggleModal}>Contact</NavLink>
+                                    </li>
+                                    <NavLink className="btn btn-secondary navbar-btn" role="button" to="/">Featured Projects</NavLink>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
         </>
-    )
+    );
 }
